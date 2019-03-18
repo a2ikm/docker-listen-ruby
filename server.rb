43c9cdaf53124b68f4fb15f6bab6862ebe512d7f
Masato Ikeda <masato.ikeda@gmail.com>
2019-03-19 01:53:13 +0900

    Run listener in docker

--- server.rb
+++ server.rb
@@ -7,6 +7,8 @@ gemfile do
   gem "listen"
 end
 
+puts "Start listening..."
+
 Listen.to(".") do |modified, added, removed|
   puts "modified absolute path: #{modified}"
   puts "added absolute path: #{added}"
